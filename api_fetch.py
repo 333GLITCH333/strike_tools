68bb3d12e602135b6898eb234a5935e5b9aa4604
333GLITCH333 <139566798+333GLITCH333@users.noreply.github.com>
2023-07-14 13:52:57 -0700

    Update api_fetch.py

--- api_fetch.py
+++ api_fetch.py
@@ -1,4 +1,3 @@
-#api_fetch.py
 import requests
 from requests.structures import CaseInsensitiveDict
 
@@ -43,44 +42,7 @@ class APIFetcher:
         url = "http://api.weatherapi.com/v1/current.json"
         params = {
             "key": "2e82b4c18182476895132928231106",
-            "q": address,#api_fetch.py
-import requests
-from requests.structures import CaseInsensitiveDict
-
-class APIFetcher:
-    @staticmethod
-    def fetch_image(url):
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.content
-        else:
-            return None
-
-    @staticmethod
-    def fetch_geocode(address):
-        url = "https://api.geoapify.com/v1/geocode/search"
-        headers = CaseInsensitiveDict()
-        headers["Accept"] = "application/json"
-        params = {
-            "text": address,
-            "apiKey": "cffbc02463704e22ba33c259df8bcf75"
-        }
-        try:
-            resp = requests.get(url, headers=headers, params=params)
-            if resp.status_code == 200:
-                return resp.json()
-            else:
-                return None
-        except requests.RequestException:
-            return None
-
-    @staticmethod
-    def fetch_ntas_alerts():
-        url = "https://api.example.com/ntas/alerts"
-        try:
-            resp = requests.get(url)
-
-            "aqi": "yes"
+            "q": address
         }
         return self.fetch_data(url, params=params)
 
