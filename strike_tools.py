cd5a7a3f17a1b4bd36064b457b75e7a767d697a7
333GLITCH333 <139566798+333GLITCH333@users.noreply.github.com>
2023-07-14 14:10:09 -0700

    Update strike_tools.py

--- strike_tools.py
+++ strike_tools.py
@@ -1,12 +1,10 @@
-#strike_tools.py
-from PyQt5.QtMultimedia import QSoundEffect
 import os
 import requests
+import webbrowser
 from PyQt5.QtCore import Qt, QSize, QUrl
 from PyQt5.QtGui import QPixmap, QFont
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QMessageBox, QApplication, QWidget, QInputDialog, QLabel, QPushButton, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QMessageBox, QApplication, QWidget, QInputDialog, QLineEdit, QLabel, QPushButton
 
-from ui_components import LogoLabel, ImageLabel, CloseAppButton, NTASAlertsButton, ImageButton, AddressInput
 from api_fetch import APIFetcher
 
 
@@ -27,17 +25,17 @@ class WeatherApp(QMainWindow):
 
         self.close_app_sound = QSoundEffect()
 
-        self.address_input = AddressInput()
+        self.address_input = QLineEdit()
 
         self.setWindowTitle("Strike Tools")
         self.setGeometry(100, 100, 400, 300)
         layout = QVBoxLayout()
-        self.logo_label = LogoLabel()
+        self.logo_label = QLabel()
         script_dir = os.path.dirname(os.path.abspath(__file__))
         logo_path = os.path.join(script_dir, "logo.jpg")
         self.logo_label.setPixmap(QPixmap(logo_path))
         layout.addWidget(self.logo_label, alignment=Qt.AlignTop)
-        self.image_label = ImageLabel()
+        self.image_label = QLabel()
         layout.addWidget(self.image_label, alignment=Qt.AlignCenter)
         image_url = "https://tripcheck.com/RoadCams/cams/Yaquina%20Bay%20Bridge%20N_pid2778.JPG"
         image_data = APIFetcher().fetch_image(image_url)
@@ -48,52 +46,37 @@ class WeatherApp(QMainWindow):
             self.image_label.setPixmap(scaled_pixmap)
 
         button_layout = QHBoxLayout()
-        self.geocode_button = ImageButton("geocode.jpg")
+        self.geocode_button = QPushButton("Geocode")
         self.geocode_button.clicked.connect(self.handle_geocode_button_click)
-        geocode_label = QLabel("Geocode")
-        button_layout.addWidget(geocode_label, alignment=Qt.AlignCenter)
         button_layout.addWidget(self.geocode_button, alignment=Qt.AlignCenter)
-        self.geocode_button.setFont(QFont("", -1, QFont.Bold))
 
-        self.maps_button = ImageButton("maps.jpg")
+        self.maps_button = QPushButton("Maps")
         self.maps_button.clicked.connect(self.handle_maps_button_click)
-        maps_label = QLabel("Maps")
-        button_layout.addWidget(maps_label, alignment=Qt.AlignCenter)
         button_layout.addWidget(self.maps_button, alignment=Qt.AlignCenter)
-        self.maps_button.setFont(QFont("", -1, QFont.Bold))
 
-        self.deals_button = ImageButton("deals.jpg")
+        self.deals_button = QPushButton("Deals")
         self.deals_button.clicked.connect(self.handle_deals_button_click)
-        deals_label = QLabel("Deals")
-        button_layout.addWidget(deals_label, alignment=Qt.AlignCenter)
         button_layout.addWidget(self.deals_button, alignment=Qt.AlignCenter)
-        self.deals_button.setFont(QFont("", -1, QFont.Bold))
 
         layout.addLayout(button_layout)
-        self.ntas_alerts_button = NTASAlertsButton()
-        self.ntas_alerts_button.setText("NTAS Alerts")
+        self.ntas_alerts_button = QPushButton("NTAS Alerts")
         self.ntas_alerts_button.clicked.connect(self.handle_ntas_alerts_button_click)
-        self.ntas_alerts_button.setFont(QFont("", -1, QFont.Bold))
         layout.addWidget(self.ntas_alerts_button)
 
         self.current_weather_button = QPushButton("Local Weather")
         self.current_weather_button.clicked.connect(self.handle_current_weather_button_click)
-        self.current_weather_button.setFont(QFont("", -1, QFont.Bold))
         layout.addWidget(self.current_weather_button)
 
         self.astro_data_button = QPushButton("Local Astro Data")
         self.astro_data_button.clicked.connect(self.handle_astro_data_button_click)
-        self.astro_data_button.setFont(QFont("", -1, QFont.Bold))
         layout.addWidget(self.astro_data_button)
 
         self.tidal_data_button = QPushButton("Local Tidal Data")
         self.tidal_data_button.clicked.connect(self.handle_tidal_data_button_click)
-        self.tidal_data_button.setFont(QFont("", -1, QFont.Bold))
         layout.addWidget(self.tidal_data_button)
 
-        self.close_app_button = CloseAppButton()
+        self.close_app_button = QPushButton("CLOSE APP")
         self.close_app_button.clicked.connect(self.close_app)
-        self.close_app_button.setFont(QFont("", -1, QFont.Bold))
         layout.addWidget(self.close_app_button, alignment=Qt.AlignBottom)
 
         central_widget = QWidget()
@@ -108,7 +91,10 @@ class WeatherApp(QMainWindow):
                 QMessageBox.warning(self, "Error", "Please enter an address.")
                 return
 
+            print("Fetching geocode data...")
             geocode_data = APIFetcher().fetch_geocode(address)
+            print(geocode_data)  # Debug print statement
+
             if geocode_data and "results" in geocode_data and len(geocode_data["results"]) > 0:
                 lon = geocode_data["results"][0]["lon"]
                 lat = geocode_data["results"][0]["lat"]
@@ -192,8 +178,9 @@ class WeatherApp(QMainWindow):
         if confirm == QMessageBox.Yes:
             self.close()
 
+
 if __name__ == "__main__":
     app = QApplication([])
     weather_app = WeatherApp()
     weather_app.show()
-    app.exec_()
+   
